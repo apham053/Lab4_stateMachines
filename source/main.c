a8ce147f5a38e43684aa5de61f60294c5c9cdd13
Anthony Pham <apham053@wch136-19.cs.ucr.edu>
2020-01-14 12:20:10 -0800

    saving

--- source/main.c
+++ source/main.c
@@ -2,8 +2,12 @@
  *  Partner(s) Name: Steven Rodriguez
  *	Lab Section: 021
  *	Assignment: Lab #4  Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
- *
+ * 
+ *	Exercise Description: PB0 and PB1 each connect to an LED, and PB0's LED 
+ *  is initially on. Pressing a button connected to PA0 turns off PB0's LED 
+ *  and turns on PB1's LED, staying that way after button release. 
+ *  Pressing the button again turns off PB1's LED and turns on PB0's LED. 
+ * 
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
@@ -12,12 +16,42 @@
 #include "simAVRHeader.h"
 #endif
 
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+   return (b ?  (x | (0x01 << k))  :  (x & ~(0x01 << k)) );
+              //   Set bit to 1           Set bit to 0
+}
+
+unsigned char GetBit(unsigned char x, unsigned char k) {
+   return ((x & (0x01 << k)) != 0);
+}
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
+	DDRA = 0x00;
+	DDRB = 0xFF;
+	PORTA = 0xFF;
+	PORTB = 0x00;
 
-    /* Insert your solution below */
-    while (1) {
-
-    }
+	unsigned char B = 0x01; //B0 is initially on
+	
+    while(1) {
+		unsigned char A = PINA & 0x01; //Read PA0
+			
+			if (GetBit(A, 0)) {        // if button pressed
+				B = SetBit(B, 0, 0);   // turn off B0
+				B = SetBit(B, 1, 1);   // turn on B1
+				
+				if (GetBit(A, 0)) {        // if button pressed
+					B = SetBit(B, 1, 0);   // turn off B1
+					B = SetBit(B, 0, 1);   // turn on B0
+				}	
+				
+			}
+			
+			PORTB = B;
+			B = 0x01;   //reset
+			
+		
+	}
+    
     return 1;
 }
--- test/tests.gdb
+++ test/tests.gdb
@@ -26,19 +26,12 @@
 echo ======================================================\n
 echo Running all tests..."\n\n
 
-# Example test:
-test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
-# Set inputs
-setPINA 0x00
-setPINB 0x00
-# Continue for several ticks
+test "PINA: 0x01, 0x00 => PORTB: 0x02"
+setPINA 0x01
 continue 2
-# Set expect values
-expectPORTC 0
-# Check pass/fail
+setPINA 0x00
+expectPORTB 0x02
 checkResult
-
-# Add tests below
 
 # Report on how many tests passed/tests ran
 set $passed=$tests-$failed
