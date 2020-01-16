796aa96952dd9134a9714b8bc2c712b18e4349be
Anthony Pham <apham053@wch136-25.cs.ucr.edu>
2020-01-16 05:21:47 -0800

    save

--- source/main.c
+++ source/main.c
@@ -16,41 +16,37 @@
 #include "simAVRHeader.h"
 #endif
 
-unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
-   return (b ?  (x | (0x01 << k))  :  (x & ~(0x01 << k)) );
-              //   Set bit to 1           Set bit to 0
+enum States { Start, state1, state2} State;
+
+void tickButton() {
+    switch(State) {
+	case Start:         // start of SM
+	    State = state1; // first state, PB0 is ON
+	    break;
+	case state1:
+	    if (A0) {      // if button pressed, go to state2
+		State = state2;
+	    }	
+	    else {         // if no button pressed, stay at state1
+		State = state1;
+	    }
+	    break;
+    }
 }
 
-unsigned char GetBit(unsigned char x, unsigned char k) {
-   return ((x & (0x01 << k)) != 0);
-}
 
 int main(void) {
 	DDRA = 0x00;
 	DDRB = 0xFF;
 	PORTA = 0xFF;
 	PORTB = 0x00;
+	State = Start;   // initial call
 
-	unsigned char B = 0x01; //B0 is initially on
+	unsigned char B = 0x01;         //B0 is initially on (LED)
+	unsigned char A0 = PINA & 0x01; //reads input A0 (button)
 	
     while(1) {
-		unsigned char A = PINA & 0x01; //Read PA0
-			
-			if (GetBit(A, 0)) {        // if button pressed
-				B = SetBit(B, 0, 0);   // turn off B0
-				B = SetBit(B, 1, 1);   // turn on B1
-				
-				if (GetBit(A, 0)) {        // if button pressed
-					B = SetBit(B, 1, 0);   // turn off B1
-					B = SetBit(B, 0, 1);   // turn on B0
-				}	
-				
-			}
-			
-			PORTB = B;
-			B = 0x01;   //reset
-			
-		
+	tickButton();	
 	}
     
     return 1;
