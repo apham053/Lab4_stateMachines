ccdc26f20d9ad391ff5855c7dd9dba6862ec1bd8
Anthony Pham <apham053@wch136-19.cs.ucr.edu>
2020-01-19 14:53:35 -0800

    save

--- source/main.c
+++ source/main.c
@@ -1,12 +1,10 @@
 /*	Author: apham053
  *  Partner(s) Name: Steven Rodriguez
  *	Lab Section: 021
- *	Assignment: Lab #4  Exercise #1
+ *	Assignment: Lab #4  Exercise #2
  * 
- *	Exercise Description: PB0 and PB1 each connect to an LED, and PB0's LED 
- *  is initially on. Pressing a button connected to PA0 turns off PB0's LED 
- *  and turns on PB1's LED, staying that way after button release. 
- *  Pressing the button again turns off PB1's LED and turns on PB0's LED. 
+ *	Exercise Description: 
+ *  
  * 
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -26,33 +24,63 @@ unsigned char GetBit(unsigned char x, unsigned char k) {
    return ((x & (0x01 << k)) != 0);
 }
 
-unsigned char B = 0x01;         //B0 is initially on (LED)
-#define A0 (PINA & 0x01)        //reads input A0 (button)
+unsigned char C = 0x07;        
+#define A (PINA & 0x03)  
 
-enum States { Start, state1, state2} State;
+enum States { Start, Wait, Add, Sub, Reset} State;
 
 void tickButton() {
     switch(State) {
-	case Start:         
-	    State = state1; 
+        case Start:         
+	    State = Wait; 
 	    break;
-	case state1:
-	    if (A0) {      
-		State = state2;
-	    }	
-	    else {         
-		State = state1;
+	case Wait: 
+	    if (A == 0x01) {
+		State = Add; 
 	    }
-	    break;
-	case state2:
-	    if (A0) {
-		State = state1;
-	    } 
+	    else if (A == 0x02) {
+		State = Sub;
+	    }
+	    else if (A == 0x03) {
+		State = Reset;
+	    }
 	    else {
-		State = state2;
+		State = Wait;
 	    }
 	    break;
-	default:
+	case Add: 
+	    if (A == 0x00) {     //release
+		if (C == 0x09) {
+		    State = Wait;
+		}
+		else {
+		    ++C;
+		    State = Wait;    
+	        }
+	    }
+	    else if (A == 0x03) {
+		State = Reset;
+	    }
+	    break;
+	case Sub:
+            if (A == 0x00) {     //release 
+                if (C == 0x00) {
+                    State = Wait;
+                }
+                else {
+                    --C;
+		    State = Wait;    
+                }
+            }
+            else if (A == 0x03) {
+                State = Reset;
+            }
+            break;
+	case Reset:
+	    C = 0x00;
+	    State = Wait;
+	    break;
+        default:
 	    State = Start;
 	    break;
     }
@@ -60,14 +88,14 @@ void tickButton() {
     switch(State) {
 	case Start:
 	    break;
-	case state1:
-	    B = SetBit(B, 0, 1);
-	    B = SetBit(B, 1, 0);
+	case Wait:
 	    break;
-	case state2:
-	    B = SetBit(B, 0, 0);
-            B = SetBit(B, 1, 1);
-	    break;
+	case Add:
+            break;
+	case Sub:
+            break;
+	case Reset:
+            break;
 	default:
 	    break;	    
     }    
@@ -75,15 +103,15 @@ void tickButton() {
 
 int main(void) {
 	DDRA = 0x00;
-	DDRB = 0xFF;
+	DDRC = 0xFF;
 	PORTA = 0xFF;
-	PORTB = 0x00;
+	PORTC = 0x00;
 	State = Start;   // initial call
 	
 	while (1) {
 	tickButton();	
-	PORTB = B;
-        B = 0x01;
+	PORTC = C;
+        C = 0x07;
 	}
     
     return 1;
